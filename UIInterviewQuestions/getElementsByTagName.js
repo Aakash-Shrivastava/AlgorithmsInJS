fd59a8649acedfcafb42199ba47c8fa477dfdae0
Aakash Shrivastava <aakash.shrivastava1@myntra.com>
2019-07-10 12:08:22 +0530

    Adding carousel and nav bar

---
+++ UIInterviewQuestions/getElementsByTagName.js
@@ -0,0 +1,15 @@
+function getElementsByAttribute(attribute){
+    var allElements = document.getElementsByTagName('*'), 
+        elm,
+        found=[];
+    for (var i = 0; i < allElements.length; i++)
+     {
+      elm = allElements[i];
+      if (elm.getAttribute(attribute))
+      {
+        found.push(elm);
+      }
+    }
+    return found;
+  }
+     
