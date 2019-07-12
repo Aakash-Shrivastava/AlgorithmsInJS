4e49b069481b5127778814b427e3ab25280dd563
Aakash Shrivastava <aakash.shrivastava1@myntra.com>
2019-07-12 15:04:25 +0530

    debounce and throttle

--- UIInterviewQuestions/genericFormValidator.js
+++ UIInterviewQuestions/genericFormValidator.js
@@ -1,12 +1,12 @@
 
 <div id="status"></div>
-    <form name="testform" id="testform" method="post" action="">
-        <p><label for="element1">first name: * <input type="text" name="element1" class="form-element" id="element1" value="" required="true" data-error="missing first name" /></label></p>
-        <p><label for="element2">last name: * <input type="text" name="element2" class="form-element" id="element2" value="" required="true" data-error="missing last name" /></label></p>
-        <p><label for="element3">email address: * <input type="text" name="element3" class="form-element" id="element3" value="" required="true" data-error="missing email address" /></label></p>
-        <p><label for="element4">phone number: * <input type="text" name="element4" class="form-element" id="element4" value="" required="true" data-error="missing phone" /></label></p>
-        <input type="button" value="Submit Form" onclick="processForm();" /> <a href="javascript:;" onclick="resetForm();">Reset</a>
-    </form>
+<form name="testform" id="testform" method="post" action="">
+    <p><label for="element1">first name: * <input type="text" name="element1" class="form-element" id="element1" value="" required="true" data-error="missing first name" /></label></p>
+    <p><label for="element2">last name: * <input type="text" name="element2" class="form-element" id="element2" value="" required="true" data-error="missing last name" /></label></p>
+    <p><label for="element3">email address: * <input type="text" name="element3" class="form-element" id="element3" value="" required="true" data-error="missing email address" /></label></p>
+    <p><label for="element4">phone number: * <input type="text" name="element4" class="form-element" id="element4" value="" required="true" data-error="missing phone" /></label></p>
+    <input type="button" value="Submit Form" onclick="processForm();" /> <a href="javascript:;" onclick="resetForm();">Reset</a>
+</form>
 
 
 #status { color: red; font-weight: bold; }
