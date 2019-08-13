0bbe718a5961939fc80bffd4aedded714ac5f91e
Aakash Shrivastava <aakash.shrivastava1@myntra.com>
2019-08-14 00:23:24 +0530

    web component changes

---
+++ MyUI/webComponents/customElementWebcomponent.js
@@ -0,0 +1,78 @@
+let tmpl = document.createElement('template');
+tmpl.innerHTML = `
+  <style>:host { ... }</style> <!-- look ma, scoped styles -->
+  <b>I'm in shadow dom!</b>
+  <slot></slot>
+`;
+
+
+class AppDrawer extends HTMLElement {
+
+    // Can define constructor arguments if you wish.
+    constructor() {
+        // If you define a constructor, always call super() first!
+        // This is specific to CE and required by the spec.
+        super();
+
+        // Setup a click listener on <app-drawer> itself.
+        this.addEventListener('click', e => {
+            // Don't toggle the drawer if it's disabled.
+            if (this.disabled) {
+                return;
+            }
+            this.toggleDrawer();
+        });
+
+        let shadowRoot = this.attachShadow({ mode: 'open' });
+        shadowRoot.appendChild(tmpl.content.cloneNode(true));
+    }
+
+
+    // adding custom element reactions
+    connectedCallback() {
+
+    }
+    disconnectedCallback() {
+
+    }
+    attributeChangedCallback(attrName, oldVal, newVal) {
+
+    }
+
+
+
+    // A getter/setter for an open property.
+    get open() {
+        return this.hasAttribute('open');
+    }
+
+    set open(val) {
+        // Reflect the value of the open property as an HTML attribute.
+        if (val) {
+            this.setAttribute('open', '');
+        } else {
+            this.removeAttribute('open');
+        }
+        this.toggleDrawer();
+    }
+
+    // A getter/setter for a disabled property.
+    get disabled() {
+        return this.hasAttribute('disabled');
+    }
+
+    set disabled(val) {
+        // Reflect the value of the disabled property as an HTML attribute.
+        if (val) {
+            this.setAttribute('disabled', '');
+        } else {
+            this.removeAttribute('disabled');
+        }
+    }
+
+    toggleDrawer() {
+    }
+}
+
+customElements.define('app-drawer', AppDrawer);
+
