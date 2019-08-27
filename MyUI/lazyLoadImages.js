2c1d8224a2ea03faf77d1ebc1ccde5ed5905edc7
Aakash Shrivastava <aakash.shrivastava1@myntra.com>
2019-08-28 01:06:54 +0530

    Changes

--- MyUI/lazyLoadImages.js
+++ MyUI/lazyLoadImages.js
@@ -1,21 +1,21 @@
-<img 
-class="lazy" src="placeholder-image.jpg" 
-data-src="image-to-lazy-load-1x.jpg" 
-data-srcset="image-to-lazy-load-2x.jpg 2x, image-to-lazy-load-1x.jpg 1x" 
-alt="I'm an image!"></img>
+<img
+  class="lazy" src="placeholder-image.jpg"
+  data-src="image-to-lazy-load-1x.jpg"
+  data-srcset="image-to-lazy-load-2x.jpg 2x, image-to-lazy-load-1x.jpg 1x"
+  alt="I'm an image!"></img>
 
 // On the document's DOMContentLoaded event, this script queries the DOM for all <img> 
 // elements with a class of lazy. If intersection observer is available, we create a new observer 
 // that runs a callback when img.lazy elements enter the viewport.
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   var lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
 
   if ("IntersectionObserver" in window) {
-    
-    let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
-      
-      entries.forEach(function(entry) {
+
+    let lazyImageObserver = new IntersectionObserver(function (entries, observer) {
+
+      entries.forEach(function (entry) {
         if (entry.isIntersecting) {
           let lazyImage = entry.target;
           lazyImage.src = lazyImage.dataset.src;
@@ -26,35 +26,31 @@ document.addEventListener("DOMContentLoaded", function() {
       });
     });
 
-    lazyImages.forEach(function(lazyImage) {
+    lazyImages.forEach(function (lazyImage) {
       lazyImageObserver.observe(lazyImage);
     });
-  } else {
-    // Possibly fall back to a more compatible method here
   }
-});
 
+  else {
+    // Possibly fall back to a more compatible method here
+    // Using Event Listeners
 
-// Using Event Listeners
-document.addEventListener("DOMContentLoaded", function() {
-    let lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
     let active = false;
-  
-    const lazyLoad = function() {
+    const lazyLoad = function () {
       if (active === false) {
         active = true;
-  
-        setTimeout(function() {
-          lazyImages.forEach(function(lazyImage) {
+
+        setTimeout(function () {
+          lazyImages.forEach(function (lazyImage) {
             if ((lazyImage.getBoundingClientRect().top <= window.innerHeight && lazyImage.getBoundingClientRect().bottom >= 0) && getComputedStyle(lazyImage).display !== "none") {
               lazyImage.src = lazyImage.dataset.src;
               lazyImage.srcset = lazyImage.dataset.srcset;
               lazyImage.classList.remove("lazy");
-  
-              lazyImages = lazyImages.filter(function(image) {
+
+              lazyImages = lazyImages.filter(function (image) {
                 return image !== lazyImage;
               });
-  
+
               if (lazyImages.length === 0) {
                 document.removeEventListener("scroll", lazyLoad);
                 window.removeEventListener("resize", lazyLoad);
@@ -62,13 +58,15 @@ document.addEventListener("DOMContentLoaded", function() {
               }
             }
           });
-  
+
           active = false;
         }, 200);
       }
     };
-  
+
     document.addEventListener("scroll", lazyLoad);
     window.addEventListener("resize", lazyLoad);
     window.addEventListener("orientationchange", lazyLoad);
-  });
+  }
+});
+
