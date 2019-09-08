7f0ecaf792e3e2a15476484249f7d9f64a6c69b5
Aakash Shrivastava <aakash.shrivastava1@myntra.com>
2019-09-08 17:26:54 +0530

    changes 2

---
+++ MyUI/polyfills/bindImplementation.js
@@ -0,0 +1,20 @@
+Function.prototype.mybind = function (...args) {
+    let context = this;
+    let previousArgs = args.slice(1);
+    return function (...currentArgs) {
+        let combinedArgs = [...previousArgs, ...currentArgs];
+        context.apply(args[0], combinedArgs);
+    }
+};
+
+let name = {
+    firstname: "Aakash",
+    lastname: "Shrivastava"
+}
+
+let printName = function (hometown, state, country) {
+    console.log(this.firstname + " " + this.lastname + " , " + hometown + ", " + state + ", " + country);
+}
+
+let printMyName2 = printName.mybind(name, "Bangaluru", "Karnataka");
+printMyName2("India");
