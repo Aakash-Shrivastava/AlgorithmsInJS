49011e7fe3865ebecf48bf9aabab8aa6b87dc491
Aakash Shrivastava <aakash.shrivastava1@myntra.com>
2019-09-22 23:38:20 +0530

    Changes

---
+++ MyUI/testFolder/AnimateNumber.js
@@ -0,0 +1,19 @@
+var initialVal = 0;
+var finalVal = 1000;
+var myNumberElem = document.getElementById("myNumber");
+
+(function () {
+	console.log("Started");
+	myNumberElem.textContent = initialVal;
+	animate(1);
+})();
+
+function animate(delay){
+  let i =initialVal;
+  let myInterval;
+  myInterval = setInterval(function(){
+    myNumberElem.textContent = i++;
+      if(i === 1001)
+      clearInterval(myInterval)
+  },delay)
+}
